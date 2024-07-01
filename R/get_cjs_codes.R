395bcdfdcf1f308a62ba6c21e412b941525c28c3
robinlovelace <rob00x@gmail.com>
2024-07-01 17:27:51 +0100

    Document

---
+++ R/get_cjs_codes.R
@@ -0,0 +1,20 @@
+#' Get Criminal Justice Codes
+#' 
+#' This function gets the criminal justice codes from the Criminal Justice Hub website.
+#' See [www.criminaljusticehub.org.uk](https://www.criminaljusticehub.org.uk/jargon-buster/cjs-offence-code/)
+#' 
+#' @export 
+#' @examples
+#' get_cjs_codes()
+get_cjs_codes = function(
+    u = paste0(
+        "https://www.criminaljusticehub.org.uk/wp-content/uploads/",
+        "2022/10/cjs-offence-index-sept-2022.ods"
+    )
+) {
+    f = file.path(tempdir(), "cjs_codes.ods")
+    message("Downloading CJS codes from ", u, " to ", f)
+    utils::download.file(u, f, mode = "wb")
+    cjs_codes = readODS::read_ods(f, sheet = 1)
+    cjs_codes
+}
